dc11b89fe40179b488fe0cf010d7decbe1469594
Ahmed Abdelkader <122270710+AhmedAbk@users.noreply.github.com>
2024-04-13 07:18:16 +0100

    fixed

--- src/components/Dest/destres.jsx
+++ src/components/Dest/destres.jsx
@@ -5,12 +5,12 @@ const Destres = () => {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [destination, setDestination] = useState(null);
+  const [Book, setBook] = useState(null);
   const [showModal, setShowModal] = useState(false);
-  const [selectedCity, setSelectedCity] = useState(' ');
+  const [selectedbook, setSelectedbook] = useState(' ');
 
   useEffect(() => {
-    const fetchDestination = async () => {
+    const fetchBook = async () => {
       try {
         const response = await fetch(`http://localhost:3001/api/books/${id}`);
         const data = await response.json();
@@ -19,48 +19,48 @@ const Destres = () => {
         console.log(data);
 
         if (response.ok) {
-          setDestination(data);
+          setBook(data);
         } else {
           setError(`Error: ${data.message || 'Failed to fetch'}`);
         }
       } catch (error) {
-        setError(`Error fetching destination: ${error.message || 'Unknown error'}`);
+        setError(`Error fetching Book: ${error.message || 'Unknown error'}`);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchDestination();
+    fetchBook();
   }, [id]);
 
-  const openModal = (city) => {
+  const openModal = (book) => {
     setShowModal(true);
-    setSelectedCity(city);
+    setSelectedbook(book);
   };
 
   const closeModal = () => {
     setShowModal(false);
   };
 
-  const getCityInformation = (city) => {
-    const { data } = destination || {};
+  const getbookInformation = (book) => {
+    const { data } = Book || {};
   
     if (data && Array.isArray(data)) { 
-      return data.map((cityData, index) => {
-        if (cityData.name === city) {
+      return data.map((bookData, index) => {
+        if (bookData.bname === book) {
           return (
             <div key={index}> 
-              <img className="img-fluid" src={cityData.bimage} alt={`${cityData.bname}`} />
+              <img className="img-fluid" src={bookData.bimage} alt={`${bookData.bname}`} />
               <div className="p-4">
                 <div className="d-flex justify-content-between mb-3">
-                  <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2" />{cityData.bname}</small> 
-                  <small className="m-0"><i className="fa fa-user text-primary mr-2" />{cityData.author}</small>
+                  <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2" />{bookData.bname}</small>
+                  <small className="m-0"><i className="fa fa-user text-primary mr-2" />{bookData.author}</small>
                 </div>
-                <a className="h5 text-decoration-none">{cityData.pdesc}</a>
+                <a className="h5 text-decoration-none">{bookData.pdesc}</a>
                 <div className="border-top mt-4 pt-4">
                   <div className="d-flex justify-content-between">
-                    <h6 className="m-0"><i className="fa fa-star text-primary mr-2" />{cityData.brating} <small>({cityData.breviews})</small></h6>
-                    <h5 className="m-0"> $ {cityData.bprice}</h5>
+                    <h6 className="m-0"><i className="fa fa-star text-primary mr-2" />{bookData.brating} <small>({bookData.breviews})</small></h6>
+                    <h5 className="m-0"> $ {bookData.prices}</h5>
                   </div>
                 </div>
               </div>
@@ -69,23 +69,23 @@ const Destres = () => {
         }
       });
     } else if (data) {
-      // one city
-      const { image, prices, description, duration, person, rating, reviews, name } = data;
+      // one  
+      const { bimage, prices, pdesc, duration, author, rating, breviews, name } = data;
   
-      if (name === city) {
+      if (name === book) {
         return (
           <>
-            <img className="img-fluid" src={image} alt={`${name}`} />
+            <img className="img-fluid" src={bimage} alt={`${name}`} />
             <div className="p-4">
               <div className="d-flex justify-content-between mb-3">
                 <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2" />{name}</small>
                 <small className="m-0"><i className="fa fa-calendar-alt text-primary mr-2" />{duration}</small>
-                <small className="m-0"><i className="fa fa-user text-primary mr-2" />{person}</small>
+                <small className="m-0"><i className="fa fa-user text-primary mr-2" />{author}</small>
               </div>
-              <a className="h5 text-decoration-none">{description}</a>
+              <a className="h5 text-decoration-none">{pdesc}</a>
               <div className="border-top mt-4 pt-4">
                 <div className="d-flex justify-content-between">
-                  <h6 className="m-0"><i className="fa fa-star text-primary mr-2" />{rating} <small>{reviews}</small></h6>
+                  <h6 className="m-0"><i className="fa fa-star text-primary mr-2" />{rating} <small>{breviews}</small></h6>
                   <h5 className="m-0">{prices}</h5>
                 </div>
               </div>
@@ -97,10 +97,10 @@ const Destres = () => {
           </>
         );
       } else {
-        return `Information about ${city}.`;
+        return `Information about ${book}.`;
       }
     } else {
-      return `Information about ${city}.`;
+      return `Information about ${book}.`;
     }
   };
 
@@ -116,21 +116,22 @@ const Destres = () => {
             <p>Loading...</p>
           ) : (
             <div className="row">
-              {destination?.data?.map((cityData, index) => (
-                <div className="col-lg-4 col-md-6 mb-4" key={index} onClick={() => openModal(cityData.name)}>
+              {Book?.data?.map((bookData, index) => (
+                <div className="col-lg-4 col-md-6 mb-4" key={index} onClick={() => openModal(bookData.bname)}>
                   <div className="package-item bg-white mb-2"> 
-                    <img className="img-fluid" src={cityData.bimage} 
+                    <img className="img-fluid" src={bookData.bimage} 
                       style={{ width: '100%', height: '250px', objectFit: 'cover' }}/>
                     <div className="p-4">
                       <div className="d-flex justify-content-between mb-3">
-                        <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2" />{cityData.bname}</small> 
-                        <small className="m-0"><i className="fa fa-user text-primary mr-2" />{cityData.author}</small>
+                        <small className="m-0"><i className="fa fa-map-marker-alt text-primary mr-2" />{bookData.bname}</small>
+                        <small className="m-0"><i className="fa fa-calendar-alt text-primary mr-2" />{bookData.duration}</small>
+                        <small className="m-0"><i className="fa fa-user text-primary mr-2" />{bookData.author}</small>
                       </div>
-                      <a className="h5 text-decoration-none"> Read {cityData.bname}</a>
+                      <a className="h5 text-decoration-none"> Visit {bookData.bname}</a>
                       <div className="border-top mt-4 pt-4">
                         <div className="d-flex justify-content-between">
-                          <h6 className="m-0"><i className="fa fa-star text-primary mr-2" />{cityData.brating} <small>({cityData.breviews})</small></h6>
-                          <h5 className="m-0">$ {cityData.bprice}</h5>
+                          <h6 className="m-0"><i className="fa fa-star text-primary mr-2" />{bookData.brating} <small>({bookData.breviews})</small></h6>
+                          <h5 className="m-0">$ {bookData.prices}</h5>
                         </div>
                       </div>
                     </div>
@@ -149,13 +150,13 @@ const Destres = () => {
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title">{selectedCity} More information</h5>
+                <h5 className="modal-title">{selectedbook} Information</h5>
                 <button type="button" className="close" onClick={closeModal}>
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
               <div className="modal-body">
-                {getCityInformation(selectedCity)}
+                {getbookInformation(selectedbook)}
                 <Link to='/Res'>
               <button className="btn btn-primary mt-3"  >
                 book now
