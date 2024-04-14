9463bee8d3973ff8d9e16f79153a40bb5facfeae
Ahmed Abdelkader <122270710+AhmedAbk@users.noreply.github.com>
2024-04-14 19:55:38 +0100

    test

--- server.js
+++ server.js
@@ -85,20 +85,16 @@ app.get('/api/books', async (req, res) => {
   }
 });
 
-// Get book by ID
-// Backend endpoint
+// Get book by ID 
 app.get("/api/bookes/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const result = await pool.query('SELECT * FROM books WHERE bid = $1', [id]);
-    
-    // Check if any book is found with the given ID
+     
     if (result.rows.length === 0) {
       return res.status(404).json({ error: 'Book not found' });
-    }
-
-    // Return the book details
-    res.json(result.rows[0]); // Assuming only one book will be found
+    } 
+    res.json(result.rows[0]);  
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ error: 'Internal Server Error' });
